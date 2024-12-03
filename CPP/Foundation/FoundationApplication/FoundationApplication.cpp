e26e1f337fc405028bf2ea40f64175220d0ddd7b
FRAPEECDN <131934335+FRAPEECDN@users.noreply.github.com>
2024-12-03 13:59:07 -0500

    Foundation - DataType (Main App)

--- CPP/Foundation/FoundationApplication/FoundationApplication.cpp
+++ CPP/Foundation/FoundationApplication/FoundationApplication.cpp
@@ -1,20 +1,74 @@
 // FoundationApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include <limits> 
+#include <math.h>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    bool b = false;
+
+    unsigned char uc = 10;
+    char c = 10;
+    // Commented code can work on some compilers but not others
+    // unsigned short us = (unsigned short)std::numeric_limits<unsigned char>::max + 1;
+    unsigned short us = 256;
+    // short s = (short)std::numeric_limits<char>::max + 1;
+    short s = 128;
+    // unsigned int ui = (unsigned int)std::numeric_limits<unsigned short>::max + 1;
+    unsigned int ui = 65536;
+    // int i = ((int)std::numeric_limits<short>::max) + 1;
+    int i = 32768;
+    unsigned long ul = 32789;
+    long l = 738347;
+    // unsigned long ul = (unsigned long)std::numeric_limits<unsigned int>::max + 1;
+    unsigned long long ull = 4294967296ULL;
+    // long l = (long)std::numeric_limits<int>::max + 1;
+    long long ll = 2147483648L;
+
+    float f = 343.12f;
+    double d = 35438580.21473;
+
+    char character = 'a';
+    std::string bString = "basic string value";
+    std::wstring bwString = L"basic string in wide character";
+
+    // Basic data tests (supported in c/c++)
+    std::cout << "Basic data types" << std::endl;
+    std::cout << "~~~~~~~~~~~~~~~~" << std::endl;
+    std::cout << "Integers" << std::endl;
+    std::cout << "~~~~~~~~~~~~~~~~" << std::endl;
+    std::cout << "Limit functions example (int type)" << std::endl;
+    std::cout << "Minimum value for int: " << std::numeric_limits<int>::min() << std::endl;
+    std::cout << "Maximum value for int: " << std::numeric_limits<int>::max() << std::endl;
+    std::cout << "Is int signed: " << std::numeric_limits<int>::is_signed << std::endl;
+    std::cout << "unsigned byte (1): " << static_cast<unsigned>(uc) << std::endl;
+    std::cout << "signed byte (1): " << static_cast<signed>(c) << std::endl;
+    std::cout << "unsigned short (2): " << us << std::endl;
+    std::cout << "signed short (2): " << s << std::endl;
+    std::cout << "unsigned int types (4): " << ui << std::endl;
+    std::cout << "signed int types (4): " << i << std::endl;
+    std::cout << "unsigned long (4): " << ul << std::endl;
+    std::cout << "signed long (4): " << l << std::endl;
+    std::cout << "unsigned long long (8): " << ull << std::endl;
+    std::cout << "signed long long (8): " << ll << std::endl;
+    std::cout << "~~~~~~~~~~~~~~~~" << std::endl;
+    std::cout << "Floating points" << std::endl;
+    std::cout << "~~~~~~~~~~~~~~~~" << std::endl;
+    std::cout << "float: " << f << std::endl;
+    std::cout << "double: " << d << std::endl;
+    std::cout << "Minimum value for double: " << std::numeric_limits<double>::min() << std::endl;
+    std::cout << "Maximum value for bouble: " << std::numeric_limits<double>::max() << std::endl;
+    std::cout << "Is double signed: " << std::numeric_limits<double>::is_signed << std::endl;
+    std::cout << "Is double infinite support: " << std::numeric_limits<double>::has_infinity << std::endl;
+    std::cout << "~~~~~~~~~~~~~~~~" << std::endl;
+    std::cout << "String and characters" << std::endl;
+    std::cout << "Basic character: " << character << std::endl;
+    std::cout << "Basic string: " << bString << std::endl;
+    std::wcout << "Wide string: " << bwString << std::endl;
+    std::cout << "~~~~~~~~~~~~~~~~" << std::endl;
+    std::cout << "Boolean" << std::endl;
+    std::cout << "boolean example: " << b << std::endl;
+    std::cout << "~~~~~~~~~~~~~~~~" << std::endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- Java/Foundation/foundations/src/main/java/com/frapee/App.java
+++ Java/Foundation/foundations/src/main/java/com/frapee/App.java
@@ -1,17 +1,81 @@
 package com.frapee;
 
-/**
- * Hello world!
- */
 public final class App {
     private App() {
     }
 
     /**
-     * Says hello to the world.
+     * Basic Data Type explenation
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        boolean b = false;
+
+        byte by = 10;
+        short s = (short) Byte.MAX_VALUE + 1;
+        int i = (int) Short.MAX_VALUE + 1;
+        long l = (long) Integer.MAX_VALUE + 1;
+
+        float f = 343.12f;
+        double d = 35438580.21473;
+
+        char ch = 'a';
+        String aStr = "basic string value";
+        
+        // boxing
+        Boolean bBox = b;
+        Byte byBox = by;        
+        Short sBox = s;
+        Integer iBox = i;
+        Long lBox = l;
+        Float fBox = f;
+        Double dBox = d;
+        Character chBox = ch;           
+
+        System.out.println("Basic data types");
+        System.out.println("~~~~~~~~~~~~~~~~");
+        System.out.println("Integers");
+        System.out.println("~~~~~~~~~~~~~~~~");
+        System.out.println("Limit functions example (int type)");
+        System.out.print("Minimum value for int: " + Integer.MIN_VALUE);
+        System.out.println();
+        System.out.print("Maximum value for int: " + Integer.MAX_VALUE);
+        System.out.println();
+        System.out.print("Int Hex function: " + Integer.toHexString(65));
+        System.out.println();
+        System.out.print("signed byte (1): " + by);
+        System.out.println(" - boxed " + byBox);
+        System.out.print("signed short (2): " + s);
+        System.out.println(" - boxed " + sBox);
+        System.out.print("signed int types (4): " + i);
+        System.out.println(" - boxed " + iBox);
+        System.out.print("signed long (8): " + l);
+        System.out.println(" - boxed " + lBox);
+        System.out.println("~~~~~~~~~~~~~~~~");
+        System.out.println("Floating points");
+        System.out.println("~~~~~~~~~~~~~~~~");
+        System.out.println();
+        System.out.print("float: " + f);
+        System.out.println(" - boxed " + fBox);
+        System.out.print("double: " + d);
+        System.out.println(" - boxed " + dBox);
+        System.out.print("Minimum value for double: " + Double.MIN_VALUE);
+        System.out.print(" Exponent :" + Double.MIN_EXPONENT);
+        System.out.println();
+        System.out.print("Maximum value for double: " + Double.MAX_VALUE);
+        System.out.print(" Exponent :" + Double.MAX_EXPONENT);
+        System.out.println();
+        System.out.println("Is double infinite support: " + Double.POSITIVE_INFINITY);
+        System.out.println("~~~~~~~~~~~~~~~~");
+        System.out.println("String and characters");
+        System.out.print("Basic character: " + ch);
+        System.out.println(" - boxed " + chBox);
+        System.out.println("Basic string: " + aStr);
+        System.out.println("~~~~~~~~~~~~~~~~");
+        System.out.println("Boolean");
+        System.out.print("boolean example: " + b);
+        System.out.println(" - boxed " + bBox);
+        System.out.println();  
+        System.out.println("~~~~~~~~~~~~~~~~");
     }
 }
